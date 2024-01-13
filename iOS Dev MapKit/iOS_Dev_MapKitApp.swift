44cd05ae3bcef2bb7e1c5e6054bbbe52391de2aa
josman185 <josman185@hotmail.com>
2024-01-13 11:54:43 -0600

    Initial Commit

---
+++ iOS Dev MapKit/iOS_Dev_MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Dev_MapKitApp.swift
+//  iOS Dev MapKit
+//
+//  Created by Jose Vargas on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Dev_MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
